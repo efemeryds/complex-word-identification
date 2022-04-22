c06c40a4e557a84f34a547758e9602cc81c0fad9
AlicjaDobrzeniecka <alizjonizm@gmail.com>
2022-04-22 17:51:31 +0200

    Merge branch 'main' of github.com:efemeryds/complex-word-identification into Alicja-Part-A

--- TODO_detailed_evaluation.py
+++ TODO_detailed_evaluation.py
@@ -5,25 +5,11 @@
 named entity recognition project for a Stanford course.
 """
 
-
-# 11. Understanding the code (1.5 Points)
-# Familiarize yourself with our version of the code and try to understand what is going on.
-# Answer in your own words (1-3 sentences per question)
-
-
-# Run the file build_vocab.py. What does this script do?
-
-
-# Inspect the file model/net.py. Which layers are being used and what is their function?
-
-
-# How could you change the loss function of the model?
-
-
-
-
+from sklearn.metrics import classification_report
+import pandas as pd
 
 # 12. Detailed evaluation (2.5 points)
+
 # Train the model on the data in preprocessed/train and preprocessed/dev by running the
 # code in train.py.
 # Evaluate the model on the data in preprocessed/test by running evaluate.py.
@@ -38,16 +24,54 @@ named entity recognition project for a Stanford course.
 
 # read the results
 
+base_model = pd.read_csv("experiments/base_model/model_output.tsv", sep='\t', header=None)
+base_model.columns = ['tokens', 'gold', 'predictions']
+frequency_model = pd.read_csv("experiments/baselines/frequency_test.csv")
+length_model = pd.read_csv("experiments/baselines/length_test.csv")
+majority_model = pd.read_csv("experiments/baselines/majority_test.csv")
+random_model = pd.read_csv("experiments/baselines/random_test.csv")
 
+final_df = pd.concat([base_model, frequency_model[['tokens', 'predictions']]], axis=1)
+final_df = pd.concat([final_df, length_model[['predictions']]], axis=1)
+final_df = pd.concat([final_df, majority_model[['predictions']]], axis=1)
+final_df = pd.concat([final_df, random_model[['predictions']]], axis=1)
 
+final_df.columns = ['tokens', 'gold', 'base_model', 'my_tokens', 'frequency_model', 'length_model', 'majority_model', 'random_model']
+
+for i in range(len(final_df)):
+    current_token = final_df['my_tokens'].iloc[i]
+    if "\n" in str(current_token):
+        final_df.iloc[i+1:, 3:] = final_df.iloc[i+1:, 3:].shift(periods=1)
+        #print("STOP")
+
+print("Loading data .. ")
 
 # implement a function for precision, recall and f1
 
+def get_report(model_name, y_true, y_pred):
+    # mapping -> 1: N, 0: C
+
+    def convert_name(label):
+        if label == "N":
+            label = 1
+        else:
+            label = 0
+        return label
+
+    y_true_list = list(y_true)
+    y_pred_list = list(y_pred)
+
+    y_true_list = list(map(convert_name, y_true_list))
+    y_pred_list = list(map(convert_name, y_pred_list))
+
+    print(model_name, " classification report")
+    print(classification_report(y_true_list, y_pred_list, labels=[1, 0]))
 
 
+model_names = ['base_model', 'frequency_model', "length_model", "majority_model", "random_model"]
 
-
-
+for name in model_names:
+    get_report(name, final_df['gold'], final_df[name])
 
 # 13. Interpretation (1.5 Points)
 # Compare the performance to the results in the shared task
